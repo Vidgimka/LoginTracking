9586516e33d8183b02004f8119227cad67aed123
Вдовиченко Владислав <vidgimka4@gmail.com>
2025-01-21 16:29:12 +0300

    Правильная структура данных для записи в БД

--- main.go
+++ main.go
@@ -12,29 +12,30 @@ import (
 )
 
 /**/
+type Data struct {
+	Login            string  `json:"login"`
+	Session_id       int     `json:"session_id"`
+	Subnet           string  `json:"subnet"`
+	Mountpoint       string  `json:"mountpoint"`
+	Station          string  `json:"station"`
+	Ntrip_agent      string  `json:"ntrip_agent"`
+	Connect_time     int     `json:"connect_time"`
+	Time_span        int     `json:"time_span"`
+	Recieved_data    float64 `json:"recieved_data"`
+	Sent_data        float64 `json:"sent_data"`
+	Status_code      int     `json:"status_code"`
+	Latency          int     `json:"latency"`
+	Sv_num           int     `json:"sv_num"`
+	Lat              float64 `json:"lat"`
+	Lon              float64 `json:"lon"`
+	Height           int     `json:"height"`
+	Station_distance float64 `json:"station_distance"`
+}
 
 type GeoData struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
-	Data    []struct {
-		Login            string  `json:"login"`
-		Session_id       int     `json:"session_id"`
-		Subnet           string  `json:"subnet"`
-		Mountpoint       string  `json:"mountpoint"`
-		Station          string  `json:"station"`
-		Ntrip_agent      string  `json:"ntrip_agent"`
-		Connect_time     int     `json:"connect_time"`
-		Time_span        int     `json:"time_span"`
-		Recieved_data    float64 `json:"recieved_data"`
-		Sent_data        float64 `json:"sent_data"`
-		Status_code      int     `json:"status_code"`
-		Latency          int     `json:"latency"`
-		Sv_num           int     `json:"sv_num"`
-		Lat              float64 `json:"lat"`
-		Lon              float64 `json:"lon"`
-		Height           int     `json:"height"`
-		Station_distance float64 `json:"station_distance"`
-	} `json:"data"`
+	Data    []Data `json:"data"`
 }
 
 func ReadFileData() GeoData { // читаем и записываем данные с API
@@ -74,7 +75,7 @@ func Init() *gorm.DB {
 		fmt.Println("не подключилось к БД")
 	}
 
-	db.AutoMigrate(&GeoData{})
+	db.AutoMigrate(&Data{})
 	return db
 }
 
