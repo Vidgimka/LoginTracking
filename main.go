a7f2e03b55e5eab690032fc385fd94dc36f88c76
Вдовиченко Владислав <vidgimka4@gmail.com>
2025-01-22 16:22:43 +0300

    Реализован скрипт добавляющий нове поел в БД

--- main.go
+++ main.go
@@ -12,12 +12,18 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 /**/
+type Response struct {
+	Login      string  `json:"login"`
+	Session_id int     `json:"session_id"`
+	Lat        float64 `json:"lat"`
+	Lon        float64 `json:"lon"`
+}
+
 type Data struct {
 	Login            string  `json:"login"`
 	Session_id       int     `json:"session_id"`
@@ -36,6 +42,7 @@ type Data struct {
 	Lon              float64 `json:"lon"`
 	Height           int     `json:"height"`
 	Station_distance float64 `json:"station_distance"`
+	//Datetime         float64 `json:"Datetime"`
 }
 
 // чтобы горм правильно определил схему, а именно теблицу DATA
@@ -70,15 +77,27 @@ func ReadFileData() GeoData { // читаем и записываем данны
 	return UsersOnline
 }
 
-func Init() *gorm.DB { // логика, как при вызове этой функци будет создаваться БД
-	var DB *gorm.DB
+func Init() *gorm.DB { // функция подключения к БД, возвращает объект подключения к БД
+	// логика, как при вызове этой функци будет создаваться БД
+	//var DB *gorm.DB
 	dsn := "host=localhost user=postgres password=postgres dbname=OnlineUsersIist port=5432 sslmode=disable"
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("не подключилось к БД")
 	}
-	DB.AutoMigrate(&Data{})
-	return DB
+	err = db.AutoMigrate(&Data{})
+	if err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	//db.AutoMigrate(&Data{})
+	//db.Migrator().DropColumn(&Data{}, "Station_distance")
+	//db.Migrator().AddColumn(&Data{}, "Stat")
+	// if err := db.Migrator().AddColumn(&Data{}, "Datetime"); err != nil {
+	// 	log.Fatalf("failed to migrate: %v", err)
+	// }
+	db.Exec("ALTER TABLE data ADD COLUMN Datetime VARCHAR(255)")
+	return db
 }
 
 /* func GetDB() *gorm.DB { // проверяем подкюченеие к базе данных
@@ -95,9 +114,13 @@ func Init() *gorm.DB { // логика, как при вызове этой фу
 // 	return Dbase
 // }*/
 
-func RunTaskEverySecond(ctx context.Context, stop <-chan struct{}) { // совмещаем логику 2х функций, Init() созданию БД
+func RunTaskEverySecond(ctx context.Context, stop <-chan struct{}) { // Запись полученных с API данных в БД
+	// совмещаем логику 2х функций, Init() созданию БД
 	// и записи ReadFileData().Data в переменную Data конкретного куска данных полученных Data с API
 	var Dbase *gorm.DB = Init()
+	// if err := Dbase.Migrator().AddColumn(&Data{}, "Datetime"); err != nil {
+	// 	log.Fatalf("failed to migrate: %v", err)
+	// }
 	ticker1 := time.NewTicker(time.Second)
 	defer ticker1.Stop()
 	for {
@@ -126,31 +149,6 @@ func GetUsersOnline(dbase *gorm.DB) []Data {
 	return usersOnline2
 }
 
-// func Getlogin(c *gin.Context) {
-// 	//var Login string
-// 	Login := c.Param("Login") // записываем в переменную вычитанный логин из URL
-// 	// запускаем цикл, который переберет все значения из переменной базы данных
-// 	// и при собпадении всех записей с заданным логином выведет их в теле ответа
-// 	for _, a := range usersOnline2 {
-// 		if a.Login == Login {
-// 			c.JSON(http.StatusOK, a)
-// 			return
-// 		}
-// 	}
-// 	c.JSON(http.StatusNotFound, gin.H{"message": "Login not found"})
-// }
-
-// func Testlogin() {
-// 	var login string
-// 	fmt.Fscan(os.Stdin, &login)
-// 	for _, a := range usersOnline2 {
-// 		if a.Login == login {
-// 			fmt.Println(a)
-// 			return
-// 		}
-// 	}
-// }
-
 func main() {
 	// реализация в основном пототке graceful shutdown
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -160,47 +158,49 @@ func main() {
 	go RunTaskEverySecond(ctx, stop) // если вынести функцию отделно, а потом
 	//вызвать горутиной, то горутины синхронизируются (Channel Synchronization)
 	// даем поработать алгоритму
-	time.Sleep(5 * time.Second) //без этого гоурутина не успевает срабоать
+	time.Sleep(3 * time.Second) //без этого гоурутина не успевает срабоать
 	close(stop)                 // закрывает горутину main
 
-	var dbase *gorm.DB = Init() // запрос делаетя один раз в main, и далее везде используется для запросов
+	//var dbase *gorm.DB = Init() // запрос делаетя один раз в main, и далее везде используется для запросов
+
 	// Блок с сервером
 
-	router := gin.Default()
+	// 	router := gin.Default()
 
-	//var dbase *gorm.DB = Init() // обращаемся к бд
-	//loginOnline := GetUsersOnline(dbase)
+	// 	//var dbase *gorm.DB = Init() // обращаемся к бд
+	// 	//loginOnline := GetUsersOnline(dbase)
 
-	router.GET("/UsersOnline2", func(c *gin.Context) {
+	// 	router.GET("/UsersOnline2", func(c *gin.Context) {
 
-		// делаем функцию, которая будет ходить в бд и возвращать данные,а не писать их в глобальную переменнюу
-		//var Online2 []Data // записываем в переменную срез элементов Data usersOnline2  данные из базы данных
-		loginOnline := GetUsersOnline(dbase)
-		//var Dbase *gorm.DB = Init() // обращаемся к бд
-		fmt.Println("Данные из БД полученны")
-		//dbase.Find(&usersOnline2) // записываем данные из БД в переменную
-		//fmt.Println(usersOnline2)
-		c.JSON(http.StatusOK, loginOnline) //c.IndentedJSON(http.StatusOK, usersOnline2)
-	})
-	//curl http://localhost:8080/UsersOnline2
+	// 		// делаем функцию, которая будет ходить в бд и возвращать данные,а не писать их в глобальную переменнюу
+	// 		//var Online2 []Data // записываем в переменную срез элементов Data usersOnline2  данные из базы данных
+	// 		loginOnline := GetUsersOnline(dbase)
+	// 		//var Dbase *gorm.DB = Init() // обращаемся к бд
+	// 		fmt.Println("Данные из БД полученны")
+	// 		//dbase.Find(&usersOnline2) // записываем данные из БД в переменную
+	// 		//fmt.Println(usersOnline2)
+	// 		c.JSON(http.StatusOK, loginOnline) //c.IndentedJSON(http.StatusOK, usersOnline2)
+	// 	})
+	// 	//curl http://localhost:8080/UsersOnline2
 
-	router.GET("/UsersOnline2/:login", func(c *gin.Context) {
-		loginOnline := GetUsersOnline(dbase)
-		fmt.Println("Данные из БД полученны")
-		dbase.Find(&loginOnline)
-		login := c.Param("login") // записываем в переменную вычитанный логин из URL
-		// запускаем цикл, который переберет все значения из переменной базы данных
-		// и при собпадении всех записей с заданным логином выведет их в теле ответа
-		for _, a := range loginOnline {
-			if a.Login == login {
-				c.JSON(http.StatusOK, a)
-				return
-			}
-		}
-		c.JSON(http.StatusNotFound, gin.H{"message": "Login not found"})
-	})
-	// //curl http://localhost:8080/UsersOnline2/login
+	// 	router.GET("/UsersOnline2/:login", func(c *gin.Context) {
+	// 		loginOnline := GetUsersOnline(dbase)
 
-	router.Run("localhost:8080")
+	// 		login := c.Param("login") // записываем в переменную вычитанный логин из URL
+	// 		// запускаем цикл, который переберет все значения из переменной баз данных
+	// 		// и при собпадении всех записей с заданным логином выведет их в теле ответа
+	// 		for _, a := range loginOnline {
+	// 			if a.Login == login {
+	// 				response := Response{a.Login, a.Session_id, a.Lat, a.Lon}
+	// 				c.JSON(http.StatusOK, response)
+	// 				return
+	// 			}
+	// 		}
+	// 		fmt.Println("Данные из БД полученны")
+	// 		c.JSON(http.StatusNotFound, gin.H{"message": "Login not found"})
 
+	// 	})
+	// 	// //curl http://localhost:8080/UsersOnline2/login
+
+	// 	router.Run("localhost:8080")
 }
